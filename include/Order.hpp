57df38d9d4027c9e8e669ff495983fd29ee4d716
notAmecarmel <hiameyaingale@gmail.com>
2026-09-21 20:47:01 +0530

    init

---
+++ include/Order.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+enum class Side {
+    BUY,
+    SELL
+};
+
+enum class OrderType {
+    LIMIT,
+    MARKET
+};
+
+struct Order {
+    uint64_t id;
+    std::string instrument;
+
+    Side side;
+    OrderType type;
+
+    double price;
+    uint64_t quantity;
+
+    uint64_t sequence;
+};
